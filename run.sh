ed0cc634aece0d7bac459d565a4fab222f1c8394
riyazkhan1432021 <86786070+riyazkhan1432021@users.noreply.github.com>
2023-03-19 00:24:39 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,1 @@
-while [ true ];
-do (npm run index.js) && echo "Server Crashed restarting in 5 seconds" && sleep 5
-done
+railway run "while :; do (node index.js) && echo 'Server Crashed restarting in 5 seconds' && sleep 5; done"
